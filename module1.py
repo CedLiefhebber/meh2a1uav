35c58262306f132b0199091274bdf13eb931302d
CedLiefhebber <ced.liefhebber@gmail.com>
2015-07-02 12:25:45 +0200

    test branch
    niet veel anders hierzo

---
+++ module1.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        hello world.py
+# Purpose:     say hello!
+#
+# Author:      Cedric
+#
+# Created:     02-07-2015
+# Copyright:   (c) Cedric 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print"hello world"
+
+if __name__ == '__main__':
+    main()
